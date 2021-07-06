3b236e2a9a666c26921cc8d1076e8fbfa5da8b6f
Stephon Ricks <stephon.ricks.01@gmail.com>
2021-07-06 17:12:38 -0500

    Created login screen

---
+++ app/src/main/java/com/example/instagram/LaunchActivity.java
@@ -0,0 +1,38 @@
+package com.example.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    public static final String TAG = "LaunchActivity";
+
+    private Button btnLogin;
+    private Button btnCreate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Takes user to the launch screen
+        setContentView(R.layout.activity_launch);
+
+        // Initial buttons
+        btnLogin = findViewById(R.id.btnLogin_Launch);
+        btnCreate = findViewById(R.id.btnCreate);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick login button");
+                Intent intent = new Intent(LaunchActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
