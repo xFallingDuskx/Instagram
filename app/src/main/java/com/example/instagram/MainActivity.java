b9f4d807d545261de5092c8b938ff5bfef208f3f
Stephon Ricks <stephon.ricks.01@gmail.com>
2021-07-07 14:19:37 -0500

    User can now log out

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.instagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    private ImageView ivLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivLogout = findViewById(R.id.ivLogout);
+
+        ivLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut(); // Log user out
+                ParseUser currentUser = ParseUser.getCurrentUser(); // User is now null
+                finish(); // navigate backwards to Login screen
+                Toast.makeText(getApplicationContext(), "You've been logged out", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
