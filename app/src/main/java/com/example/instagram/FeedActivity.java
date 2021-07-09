8d77de20a3eec31d20939aa75eed4f605e144d00
Stephon Ricks <stephon.ricks.01@gmail.com>
2021-07-09 16:29:52 -0500

    Set up Home feed fragment

--- app/src/main/java/com/example/instagram/FeedActivity.java
+++ app/src/main/java/com/example/instagram/FeedActivity.java
@@ -1,91 +1,53 @@
 package com.example.instagram;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
+import android.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseQuery;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
-import org.json.JSONArray;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.example.instagram.fragments.FeedFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class FeedActivity extends AppCompatActivity {
     
     public static final String TAG = "FeedActivity";
-    private SwipeRefreshLayout swipeContainer;
 
-    RecyclerView rvPosts;
-    List<Post> allPosts;
-    PostsAdapter adapter;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
-        rvPosts = findViewById(R.id.rvPosts);
-        allPosts = new ArrayList<>();
-        adapter = new PostsAdapter(this, allPosts);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        rvPosts.setLayoutManager(linearLayoutManager);
-        rvPosts.setAdapter(adapter);
-        
-        displayPosts();
-
-        // Lookup the swipe container view
-        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
-        // Setup refresh listener which triggers new data loading
-        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onRefresh() {
-                fetchTimelineAsync(0);
-                swipeContainer.setRefreshing(false);
-            }
-        });
-        // Configure the refreshing colors
-        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light,
-                android.R.color.holo_orange_light,
-                android.R.color.holo_red_light);
-    }
-
-    private void fetchTimelineAsync(int i) {
-        adapter.clear();
-        displayPosts();
-    }
-
-
-    private void displayPosts() {
-        // Specify the type of data we desire
-        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
-        // Include additional data (the user)
-        query.include(Post.KEY_USER);
-        // Set limit to query
-        query.setLimit(20);
-        // Order posts by creation data
-        query.addDescendingOrder("createdAt");
-        // Async call for posts
-        query.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> posts, ParseException e) {
-                if (e != null) {
-                    Log.e(TAG, "Issue getting posts", e);
-                    return;
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FeedFragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        fragment = new FeedFragment();
+                        Toast.makeText(FeedActivity.this, "Home!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_compose:
+                        Toast.makeText(FeedActivity.this, "Compose!", Toast.LENGTH_SHORT).show();
+                        fragment = new FeedFragment();
+                        break;
+                    case R.id.action_profile:
+                        Toast.makeText(FeedActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
+                        fragment = new FeedFragment();
+                        break;
+                    default:
+                        fragment = new FeedFragment();
+                        break;
                 }
-                // For debugging
-                for (Post post : posts) {
-                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
-                }
-                allPosts.addAll(posts);
-                adapter.notifyDataSetChanged();
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
             }
         });
     }
---
+++ app/src/main/java/com/example/instagram/fragments/FeedFragment.java
@@ -0,0 +1,146 @@
+package com.example.instagram.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.instagram.Post;
+import com.example.instagram.PostsAdapter;
+import com.example.instagram.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FeedFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FeedFragment extends Fragment {
+
+    private static final String TAG = "FeedFragment";
+    private SwipeRefreshLayout swipeContainer;
+
+    RecyclerView rvPosts;
+    List<Post> allPosts;
+    PostsAdapter adapter;
+
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FeedFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FeedFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FeedFragment newInstance(String param1, String param2) {
+        FeedFragment fragment = new FeedFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvPosts = view.findViewById(R.id.rvPosts);
+        allPosts = new ArrayList<>();
+        adapter = new PostsAdapter(getContext(), allPosts);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        rvPosts.setLayoutManager(linearLayoutManager);
+        rvPosts.setAdapter(adapter);
+
+        displayPosts();
+
+        // Lookup the swipe container view
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchTimelineAsync(0);
+                swipeContainer.setRefreshing(false);
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+    }
+
+    private void fetchTimelineAsync(int i) {
+        adapter.clear();
+        displayPosts();
+    }
+
+
+    private void displayPosts() {
+        // Specify the type of data we desire
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        // Include additional data (the user)
+        query.include(Post.KEY_USER);
+        // Set limit to query
+        query.setLimit(20);
+        // Order posts by creation data
+        query.addDescendingOrder("createdAt");
+        // Async call for posts
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue getting posts", e);
+                    return;
+                }
+                // For debugging
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
